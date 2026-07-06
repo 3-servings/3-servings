6a04bed4931fc75cafcd5cdd3c773f2730913de5
joonseo21 <joonseo2012@g.skku.edu>
2026-07-06 11:28:49 +0900

    init commit

---
+++ src/main/java/com/sparta/server/threeserving/ThreeServingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.server.threeserving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeServingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeServingApplication.class, args);
+    }
+
+}
