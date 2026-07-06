b30abba8f6ae3357fdcdfc27c2177f5dff1a6524
joonseo21 <joonseo2012@g.skku.edu>
2026-07-06 12:00:57 +0900

    Revert "프로구조 추가"
    This reverts commit 90316d2b24c074541f6c1e2acf83493cc9a5ebd7.

--- src/main/java/com/sparta/server/threeserving/order/entity/OrderTypeEnum.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.server.threeserving.order.entity;
-
-public enum OrderTypeEnum {
-    ONLINE,	    // 온라인 주문
-}
--- src/main/java/com/sparta/server/threeserving/order/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.server.threeserving.order.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrderService {
-
-}
