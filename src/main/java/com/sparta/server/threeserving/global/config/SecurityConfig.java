07cbbde13510b1dce865730b7b4b345aa32248c5
joonseo21 <joonseo2012@g.skku.edu>
2026-07-09 21:36:09 +0900

    Fix&Refactor(Order) 피드백 적용

--- src/main/java/com/sparta/server/threeserving/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/server/threeserving/global/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.sparta.server.threeserving.global.config;
 
-import com.sparta.server.threeserving.auth.cookie.CookieUtil;
+import com.sparta.server.threeserving.auth.UserDetailsServiceImpl;
 import com.sparta.server.threeserving.auth.jwt.JwtAuthenticationFilter;
 import com.sparta.server.threeserving.auth.jwt.JwtAuthorizationFilter;
 import com.sparta.server.threeserving.auth.jwt.JwtUtil;
-import com.sparta.server.threeserving.auth.UserDetailsServiceImpl;
 import com.sparta.server.threeserving.auth.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -63,6 +62,7 @@ public class SecurityConfig {
             // 공개 API
             .requestMatchers("/signup").permitAll()
             .requestMatchers("/api/user/**").permitAll()
+            .requestMatchers("/api/carts/**").hasAnyRole("CUSTOMER", "MASTER", "MANAGER")
             .anyRequest().permitAll()     // 모든 요청 허용
         );
 
