0149837bdbed6655406f02132656a90c588ef3b6
nadaa <nadaa@ajou.ac.kr>
2026-07-09 16:34:24 +0900

    feat: refreshToken 재발급 추가

--- src/main/java/com/sparta/server/threeserving/user/controller/UserController.java
+++ src/main/java/com/sparta/server/threeserving/user/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.sparta.server.threeserving.user.controller;
 
+import com.sparta.server.threeserving.auth.jwt.JwtUtil;
+import com.sparta.server.threeserving.auth.jwt.TokenService;
+import com.sparta.server.threeserving.global.common.response.ApiResponse;
+import com.sparta.server.threeserving.global.common.response.SuccessCode;
 import com.sparta.server.threeserving.user.dto.SignupRequest;
 import com.sparta.server.threeserving.user.dto.UserResponse;
 import com.sparta.server.threeserving.user.entity.UserRoleEnum;
 import com.sparta.server.threeserving.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +20,11 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final TokenService tokenService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TokenService tokenService) {
         this.userService = userService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/signup/customer")
@@ -30,4 +39,19 @@ public class UserController {
             @Valid @RequestBody SignupRequest request) {
         return ResponseEntity.ok(userService.signup(request, UserRoleEnum.OWNER));
     }
+
+
+    @PostMapping("/re-issue")
+    public ResponseEntity<ApiResponse<Void>> reissue(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        String accessToken = tokenService.reissueAccessToken(request);
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
+
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.SUCCESS));
+    }
+
+    
 }
