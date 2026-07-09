42cb8a0fd2914054749d22f205a07c33222282d6
nadaa <nadaa@ajou.ac.kr>
2026-07-09 16:30:59 +0900

    feat: getCookie() 구현

--- src/main/java/com/sparta/server/threeserving/auth/cookie/CookieUtil.java
+++ src/main/java/com/sparta/server/threeserving/auth/cookie/CookieUtil.java
@@ -4,6 +4,9 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class CookieUtil {
 
 
@@ -28,6 +31,16 @@ public class CookieUtil {
                 }
             }
         }
+    }
+
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        if (request.getCookies() == null) {
+            return Optional.empty();
+        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(name))
+                .findFirst();
     }
 
 
