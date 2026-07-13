4f8d5a717079cfb22e6c7cd0fcd926d9f5651561
leunbin <silverbin2105@gmail.com>
2026-07-13 14:14:28 +0900

    Fix(Payments) : 주석 해제

--- src/main/java/com/sparta/server/threeserving/payment/service/PaymentService.java
+++ src/main/java/com/sparta/server/threeserving/payment/service/PaymentService.java
@@ -127,9 +127,9 @@ public class PaymentService {
                 )
         );
 
-//        orderManagementService.create(
-//                new OrderManagementCreateRequest(savedPayment.getOrder().getId())
-//        );
+        orderManagementService.create(
+                new OrderManagementCreateRequest(savedPayment.getOrder().getId())
+        );
 
         return PaymentResponse.from(savedPayment);
     }
