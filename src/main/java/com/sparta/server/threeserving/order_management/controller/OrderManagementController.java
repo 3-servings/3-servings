1e48d19f046c10cfee6e44448d5881fc3d8c3591
juwonyoung <dnjsdud0702@naver.com>
2026-07-14 15:36:22 +0900

    Feat : Store권한 구분 추가

--- src/main/java/com/sparta/server/threeserving/order_management/controller/OrderManagementController.java
+++ src/main/java/com/sparta/server/threeserving/order_management/controller/OrderManagementController.java
@@ -94,20 +94,39 @@ public class OrderManagementController {
     }
 
     @GetMapping("/stores/{storeId}/dashboard/summary")
-    public ApiResponse<TodaySalesSummaryResponse> getDashboardSummary(@PathVariable UUID storeId) {
-        TodaySalesSummaryResponse response = dailySalesStatService.getTodaySummary(storeId);
+    public ApiResponse<TodaySalesSummaryResponse> getDashboardSummary(
+            @PathVariable UUID storeId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        TodaySalesSummaryResponse response = dailySalesStatService.getTodaySummary(
+                storeId,
+                userDetails.getUser().getId(),
+                userDetails.getUser().getRole());
         return ApiResponse.success(SUCCESS, response);
     }
 
     @GetMapping("/stores/{storeId}/dashboard/trend")
-    public ApiResponse<DashboardTrendResponse> getDashboardTrend(@PathVariable UUID storeId, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
-        DashboardTrendResponse response = dailySalesStatService.getDashboardTrend(storeId, startDate, endDate);
+    public ApiResponse<DashboardTrendResponse> getDashboardTrend(
+            @PathVariable UUID storeId,
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalDate endDate,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        DashboardTrendResponse response = dailySalesStatService.getDashboardTrend(
+                storeId,
+                startDate,
+                endDate,
+                userDetails.getUser().getId(),
+                userDetails.getUser().getRole());
         return ApiResponse.success(SUCCESS, response);
     }
 
     @GetMapping("/stores/{storeId}/dashboard/reject-reasons")
-    public ApiResponse<RejectReasonStatResponse> getRejectReasonStatistics(@PathVariable UUID storeId) {
-        RejectReasonStatResponse response = dailySalesStatService.getRejectReasonStatistics(storeId);
+    public ApiResponse<RejectReasonStatResponse> getRejectReasonStatistics(
+            @PathVariable UUID storeId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        RejectReasonStatResponse response = dailySalesStatService.getRejectReasonStatistics(
+                storeId,
+                userDetails.getUser().getId(),
+                userDetails.getUser().getRole());
         return ApiResponse.success(SUCCESS, response);
     }
 }
