3cec5cc2d8172aacbbca2dc4698fab5c2152bacd
leunbin <silverbin2105@gmail.com>
2026-07-14 16:31:25 +0900

    Feat(Payment: 토스 결제 승인 및 결제 플로우 개선

--- src/main/java/com/sparta/server/threeserving/global/config/RestClientConfig.java
+++ src/main/java/com/sparta/server/threeserving/global/config/RestClientConfig.java
@@ -1,7 +1,11 @@
 package com.sparta.server.threeserving.global.config;
 
+import com.sparta.server.threeserving.global.common.exception.ErrorCode;
+import com.sparta.server.threeserving.global.exception.CustomException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +13,14 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.create();
+        SimpleClientHttpRequestFactory factory =
+                new SimpleClientHttpRequestFactory();
+
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(5000);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 }
